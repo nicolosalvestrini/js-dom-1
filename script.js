d05723a47203f6705666047f5a0d3c4db81c23e9
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2025-12-30 17:22:19 +0100

    add function on off

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const immagineLampadinaSpenta = querySelector("img.spenta");
-const immagineLamadinaAccesa = querySelector("img.d-none")
-const bottoneAccendiSpegni = querySelector("button.btn")
+const immagineLampadinaSpenta = document.querySelector("img.spenta");
+const immagineLamadinaAccesa = document.querySelector("img.d-none");
+const bottoneAccendiSpegni = document.querySelector("button.btn");
+const immagineEasteregg =document.querySelector("img.easteregg");
+let contatoreClick = 0;
 
+bottoneAccendiSpegni.addEventListener("click", function interrutore() {
+
+    const lampadinaSpenta = immagineLamadinaAccesa.classList.contains("d-none");
+    if (lampadinaSpenta) {
+        immagineLampadinaSpenta.classList.remove("spenta")
+        immagineLampadinaSpenta.classList.add("d-none")
+        immagineLamadinaAccesa.classList.remove("d-none")
+        bottoneAccendiSpegni.innerText = "Spegni"
+    } else {
+        immagineLampadinaSpenta.classList.add("spenta")
+        immagineLampadinaSpenta.classList.remove("d-none")
+        immagineLamadinaAccesa.classList.add("d-none")
+        bottoneAccendiSpegni.innerText = "Accendi"
+    }   
+});
